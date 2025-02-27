be70059f44421b827a9abb4aa7199bb40fae24c1
yeshaswini-bhoomi <yeshubhoomi@gmail.com>
2025-02-27 15:09:19 +0530

    yehsu

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const express = require("express");
 //6. import dotenv
 const dotenv = require("dotenv");
 //10. import other dependencies
-const colors = require('colors')
-const morgan = require("morgan")
+const colors = require("colors");
+const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
@@ -20,13 +20,12 @@ connectDB();
 const app = express();
 
 //11. middlewares - next function
-app.use(express.json());  //2. app can access json responses also
-app.use(cors());          //1. enables cors
-app.use(morgan("dev"));  //3. receive a message on the console that which url is active and what the response is and what time it took
-
+app.use(express.json()); //2. app can access json responses also
+app.use(cors()); //1. enables cors
+app.use(morgan("dev")); //3. receive a message on the console that which url is active and what the response is and what time it took
 
 //5. routes
-//1 test route - use get method 
+//1 test route - use get method
 //'/' indicates local host home route and then we use call back function which has request,response and middleware req is used to take request from the user and res to give some response from the user
 //request - we take request from user response - json response can also send html response
 //first we used get method now we are using use method it acts as middleware
@@ -43,7 +42,8 @@ const PORT = process.env.PORT || 8080;
 
 //4. create a method named listen to call to run our app
 app.listen(PORT, () => {
-    console.log(
-      `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}` .bgBlue.white
-    );
-  })
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
+      .bgBlue.white
+  );
+});
